4ffd4195ccabe10763311d19d6fff09cf911026e
Benjas <benjaminrodriguezleyva@imac-de-benjas.gateway.2wire.net>
2011-10-31 15:26:42 -0600

    Agregado inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App1::Application.config.secret_token = 'e775ee7ba03c0acc0794a2d2091776ff55fcad00052acf18efaa6a2c394da84b9ee49fc450b71f84434485f4a8dc6741b0cec6c452b666c6d04924d8e7435224'
